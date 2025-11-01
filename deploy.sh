362460efc5c25d17423b937557ea880b1816b1f2
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-11-02 00:58:24 +0100

    updated deployment

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DOMAIN="borgarbarland.com"
+PORT=2808
+
+COMMIT_HASH=$(git rev-parse --short HEAD)
+IMAGE_NAME="$DOMAIN:$COMMIT_HASH"
+
+echo "-> Building new Docker image"
+docker build -t $IMAGE_NAME .
+
+echo "-> Stopping and removing old container"
+docker rm -f $DOMAIN || true
+
+echo "-> Starting new container"
+docker run -p $PORT:3000 --name $DOMAIN --restart unless-stopped -d $IMAGE_NAME
